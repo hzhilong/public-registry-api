cabeb264c56f94f1b8cef3bdfa232990808e772b
hzhilong <hzhilong777@outlook.com>
2026-05-04 05:45:17 +0800

    更新构建打包配置；

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
-import { defineConfig, ConfigEnv } from 'vite'
+import { defineConfig, type ConfigEnv } from 'vite'
 import dts from 'vite-plugin-dts'
 import path from 'node:path'
-import pkg from './package.json'
 
 export default defineConfig((_configEnv: ConfigEnv) => {
   return {
@@ -10,7 +9,7 @@ export default defineConfig((_configEnv: ConfigEnv) => {
     plugins: [
       // 生成 .d.ts 类型文件
       dts({
-        tsconfigPath: 'tsconfig.json',
+        tsconfigPath: 'tsconfig.dev.json',
         outDir: 'dist',
         entryRoot: 'src',
         // 将所有声明合并到一个文件中
@@ -27,7 +26,7 @@ export default defineConfig((_configEnv: ConfigEnv) => {
           index: path.resolve(__dirname, 'src/index.ts'),
         },
         // 库的名称，会作为全局变量名使用
-        name: pkg.name,
+        name: 'public-registry-api',
         formats: ['es', 'cjs'],
         fileName: (format, entryName) => {
           if (format === 'es') {
