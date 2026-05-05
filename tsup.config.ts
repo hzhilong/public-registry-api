afe8586786b6fc67d9af1f42bafc793f70807f4e
hzhilong <hzhilong777@outlook.com>
2026-05-05 22:44:52 +0800

    使用 tsup 构建项目

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,24 +1,21 @@
 import { defineConfig } from 'tsup'
 import { resolve } from 'path'
-import { builtinModules } from 'node:module'
 
 const root = resolve(import.meta.dirname, './')
 
 export default defineConfig((options) => {
   return {
+    platform: 'neutral',
     entry: {
       index: resolve(root, './src/index.ts'),
     },
     outDir: 'dist',
     format: ['esm', 'cjs'],
     sourcemap: true,
-    // 类型生成
     dts: true,
     clean: true,
     minify: !options.watch,
-    // 禁用代码分割，确保每个入口是独立文件
     splitting: false,
-    external: [...builtinModules, /^node:/],
     tsconfig: resolve(root, `tsconfig.dev.json`),
     // 修正产物名 (tsup 默认会带上 .mjs / .cjs)
     outExtension({ format }) {
